5de80c0020fd031ac45dce7b541d92a67879b767
Maxim Kazantsev <aeontal@gmail.com>
2021-02-28 17:37:54 +0500

    Реформат кода

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
-    public static void main(String[] args)
-    {   double balance = 100;
+    public static void main(String[] args) {
+        double balance = 100;
         double replenishment = 1100;
-        double bonus = replenishment>1000?Math.floor(replenishment/100):0;
-        double total_balance = balance+replenishment+bonus;
+        double bonus = replenishment > 1000 ? Math.floor(replenishment / 100) : 0;
+        double total_balance = balance + replenishment + bonus;
         System.out.println(total_balance);
     }
 
